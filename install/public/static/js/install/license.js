91dd6e27cd2e192cb41c3a4bc115c209dbdca362
chekun <234267695@qq.com>
2013-03-23 12:02:30 +0800

    调整install/license.js中container为类成员变量

--- install/public/static/js/install/license.js
+++ install/public/static/js/install/license.js
@@ -1,25 +1,25 @@
 define(["jquery", 'jquery-ui', "text!templates/license.html"], function($, ui, tpl) {
-        var container = $('#step-license');
         var License = function() {
             this.isShow = false;
+            this.$container = $('#step-license');
             this.init();
             this.show();
         }
         //init html
         License.prototype.init = function() {
-            container.html(tpl);
-            container.find('.checkbox-custom').checkbox();
-            container.find('.checkbox-custom').on('changed', function (e, data) {
+            this.$container.html(tpl);
+            this.$container.find('.checkbox-custom').checkbox();
+            this.$container.find('.checkbox-custom').on('changed', function (e, data) {
                 if (data.isChecked)
                 {
-                    container.find('.modal-footer button').removeClass('disabled');
+                    this.$container.find('.modal-footer button').removeClass('disabled');
                 }
                 else
                 {
-                    container.find('.modal-footer button').addClass('disabled');
+                    this.$container.find('.modal-footer button').addClass('disabled');
                 }
             });
-            container.find('.modal-footer button').click(function(){
+            this.$container.find('.modal-footer button').click(function(){
                 window.wizard.wizard('next');
             });
         }
@@ -27,15 +27,15 @@ define(["jquery", 'jquery-ui', "text!templates/license.html"], function($, ui, t
         License.prototype.show = function() {
             if ( ! this.isShow)
             {
-                container.find('.modal-body').html('DiliCMS是开源的，面向CodeIgniter开发者的，自由灵活的后台系统，并致力于为开发者提供最简单，易扩展，实用的后台系统。');
+                this.$container.find('.modal-body').html('DiliCMS是开源的，面向CodeIgniter开发者的，自由灵活的后台系统，并致力于为开发者提供最简单，易扩展，实用的后台系统。');
                 this.isShow = true;
             }
         }
         //on change, check 
         License.prototype.change = function(e) {
-            if ( ! container.find('.checkbox-custom').checkbox('isChecked'))
+            if ( ! this.$container.find('.checkbox-custom').checkbox('isChecked'))
             {
-                $(container).effect('shake');
+                this.$container.effect('shake');
                 e.preventDefault();
             }
         }
