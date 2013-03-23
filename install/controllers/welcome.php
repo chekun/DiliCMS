7ae4f10fc14527d74fb8c34bc4860ec0ae3d3d68
chekun <234267695@qq.com>
2013-03-23 11:40:09 +0800

    增加install文件夹，并导入CI application结构

---
+++ install/controllers/welcome.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('welcome_message');
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
