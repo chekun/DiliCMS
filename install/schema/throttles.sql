12c179ecdf56ff3851d1c7646897fc79613d1c67
chekun <234267695@qq.com>
2014-03-19 21:58:15 +0800

    add qrcode on install page

---
+++ install/schema/throttles.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS `{DB_PREFIX}throttles`{SEPERATOR}
+CREATE TABLE `{DB_PREFIX}throttles` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `type` varchar(255) NOT NULL,
+  `ip` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8{SEPERATOR}
+DELETE FROM `{DB_PREFIX}throttles`
